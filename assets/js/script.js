6991f9a130f8a9eac5674606a2bf3b306272c54f
Sandipdonda21 <sandipdonda2121@gmail.com>
2022-01-28 11:51:49 +0530

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
 const next_form = (cdiv,prn) =>{
+    if(cdiv!=4 || prn!=66){
     document.getElementById(`f_${cdiv}`).style.display = 'none';
     document.getElementById(`f_${cdiv+1}`).style.display = 'block';
     document.getElementById('pr').style.width = `${prn+33}%`;
     let element1 = document.querySelector(`#l${cdiv}`);
     element1.classList.remove('active');
     let element2 = document.querySelector(`#l${cdiv+1}`);
-    element2.classList.add('active');
+    element2.classList.add('active');}
+    else{
+        window.location.replace('dashboard.html');
+    }
+
 }
 const pre_form = (cdiv ,prn) =>{
     document.getElementById(`f_${cdiv}`).style.display = 'none';
